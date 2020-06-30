b0b6cb629ef1884c7d7b4f621426ae99980ac7b4
paulwoojungkim <wj2kim@gmail.com>
2020-07-01 02:58:16 +0900

    split tutorial

--- spliting-sample/src/App.js
+++ spliting-sample/src/App.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import notify from "./notify";
 
-function App() {
-  const onClick = () => {
-    notify();
+class App extends Component {
+  state = {
+    SplitMe: null,
   };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React!</p>
-        <button value="click me" onClick={onClick}></button>
-      </header>
-    </div>
-  );
+
+  handleClick = async () => {
+    const loadedModule = await import("./SplitMe");
+    this.setState({
+      SplitMe: loadedModule.default,
+    });
+  };
+
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//   const onClick = () => {
+//     import("./notify").then((result) => result.default());
+//   };
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello React!</p>
+//         <button value="click me" onClick={onClick}></button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ spliting-sample/src/SplitMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpliteMe = () => {
+  return <div>SplitMe</div>;
+};
+
+export default SpliteMe;
