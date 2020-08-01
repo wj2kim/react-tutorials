07fd7a99046a193a0801b4cb74cbb106f4614d7e
paulwoojungkim <wj2kim@gmail.com>
2020-08-01 21:55:47 +0900

    working on css-animation

---
+++ css-animation/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
+
+const App = () => {
+  return (
+    <NavBar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />}>
+        {
+          // Drop down goes here
+        }
+      </NavItem>
+    </NavBar>
+  );
+};
+
+const NavBar = (props) => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+};
+
+const NavItem = (props) => {
+  return (
+    <li className="nav-item">
+      <a href="/" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  );
+};
+
+export default App;
