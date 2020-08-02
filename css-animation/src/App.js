e93a4a99da2286e0988e0cabf2952c6a99992e3d
paulwoojungkim <wj2kim@gmail.com>
2020-08-02 17:30:46 +0900

    working on css-animation

--- css-animation/src/App.js
+++ css-animation/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as BellIcon } from "./icons/bell.svg";
 import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
 import { ReactComponent as CaretIcon } from "./icons/caret.svg";
@@ -7,6 +7,10 @@ import { ReactComponent as CogIcon } from "./icons/cog.svg";
 import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
 import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
 import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
+
+/* helps control conditional logic for rendering multiple menus 
+and transitioning between them when they are added or removed from application */
+import { CSSTransition } from "react-transition-group";
 
 const App = () => {
   return (
@@ -17,6 +21,7 @@ const App = () => {
       <NavItem icon={<CaretIcon />}>
         {
           // Drop down goes here
+          <DropdownMenu />
         }
       </NavItem>
     </NavBar>
@@ -32,12 +37,35 @@ const NavBar = (props) => {
 };
 
 const NavItem = (props) => {
+  const [open, setOpen] = useState(false);
   return (
     <li className="nav-item">
-      <a href="/" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+      {open && props.children}
     </li>
+  );
+};
+
+const DropdownMenu = () => {
+  const DropdownItem = (props) => {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  };
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />}
+      ></DropdownItem>
+    </div>
   );
 };
 
